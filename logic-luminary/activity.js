2b3bc32afb75abca6144c5a36ed6d4c60c0835a0
Thomas J Bradley <hey@thomasjbradley.ca>
2016-02-29 21:04:54 -0500

    Add the logic luminary quiz

---
+++ logic-luminary/activity.js
@@ -0,0 +1,96 @@
+var questions = [
+  {
+    question: 'Which path will be executed?',
+    extras: 'question-1',
+    correct: 0,
+    choices: [
+      'True path',
+      'False path'
+    ]
+  },
+  {
+    question: 'Which path will be executed?',
+    extras: 'question-2',
+    correct: 1,
+    choices: [
+      'True path',
+      'False path'
+    ]
+  },
+  {
+    question: 'Which path will be executed?',
+    extras: 'question-3',
+    correct: 0,
+    choices: [
+      'True path',
+      'False path'
+    ]
+  },
+  {
+    question: 'Which path will be executed?',
+    extras: 'question-4',
+    correct: 0,
+    choices: [
+      'True path',
+      'False path'
+    ]
+  },
+  {
+    question: 'Which path will be executed?',
+    extras: 'question-5',
+    correct: 0,
+    choices: [
+      'True path',
+      'False path'
+    ]
+  },
+  {
+    question: 'Which path will be executed?',
+    extras: 'question-6',
+    correct: 1,
+    choices: [
+      'True path',
+      'False path'
+    ]
+  },
+  {
+    question: 'Which path will be executed?',
+    extras: 'question-7',
+    correct: 1,
+    choices: [
+      'True path',
+      'False path'
+    ]
+  },
+  {
+    question: 'Which path will be executed?',
+    extras: 'question-8',
+    correct: 2,
+    choices: [
+      'Path 1',
+      'Path 2',
+      'Path 3',
+      'Path 4'
+    ]
+  },
+  {
+    question: 'Which path will be executed?',
+    extras: 'question-9',
+    correct: 1,
+    choices: [
+      'True path',
+      'False path'
+    ]
+  },
+  {
+    question: 'Which path will be executed?',
+    extras: 'question-10',
+    correct: 1,
+    choices: [
+      'True path',
+      'False path'
+    ]
+  }
+];
+
+MultipleChoice(ActivityRunner(), questions);
