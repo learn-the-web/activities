45677802fedf949f263648eb80023706f0e332ce
Thomas J Bradley <hey@thomasjbradley.ca>
2016-04-08 11:45:12 -0400

    Add the Acronym Annihilator activity

---
+++ acronym-annihilator/activity.js
@@ -0,0 +1,112 @@
+var questions = [
+  {
+    question: 'HTML:',
+    correct: 1,
+    choices: [
+      'HyperText Markup Letters',
+      'HyperText Markup Language',
+      'Hyper Transformation Multiple Languages'
+    ]
+  },
+  {
+    question: 'CSS:',
+    correct: 0,
+    choices: [
+      'Cascading Style Sheets',
+      'Colouring & Styling Sheets',
+      'Cascading Styling Scripts'
+    ]
+  },
+  {
+    question: 'JS:',
+    correct: 2,
+    choices: [
+      'Johnson Space',
+      'Java Semantics',
+      'Javascript'
+    ]
+  },
+  {
+    question: 'URL:',
+    correct: 0,
+    choices: [
+      'Uniform Resource Locator',
+      'Underworld Reading Lounge',
+      'Underline Real Letters'
+    ]
+  },
+  {
+    question: 'HTTP:',
+    correct: 1,
+    choices: [
+      'HyperTension Transfer Point',
+      'HyperText Transfer Protocol',
+      'Hyper Train Transfer Point'
+    ]
+  },
+  {
+    question: 'HTTPS:',
+    correct: 2,
+    choices: [
+      'HyperText Transfer Protocol Version “S”',
+      'HyperTension Timing Placement System',
+      'Secure HyperText Transfer Protocol'
+    ]
+  },
+  {
+    question: 'SVG:',
+    correct: 0,
+    choices: [
+      'Scalable Vector Graphics',
+      'Super Vector Graphics',
+      'Scalable Velocity Gaming'
+    ]
+  },
+  {
+    question: 'PNG:',
+    correct: 1,
+    choices: [
+      'Joint Photographic Experts Group',
+      'Portable Network Graphics',
+      'Graphics Interchange Format'
+    ]
+  },
+  {
+    question: 'WWW:',
+    correct: 0,
+    choices: [
+      'World Wide Web',
+      'Web Wonder Woman',
+      'Wide World Web'
+    ]
+  },
+  {
+    question: 'DNS:',
+    correct: 2,
+    choices: [
+      'Domain Numeral Server',
+      'Domain Name Security',
+      'Domain Name System'
+    ]
+  },
+  {
+    question: 'TLD:',
+    correct: 2,
+    choices: [
+      'Tiny Little Domain',
+      'Top Last Domain',
+      'Top Level Domain'
+    ]
+  },
+  {
+    question: 'W3C:',
+    correct: 1,
+    choices: [
+      'World Wide Web Controllers',
+      'World Wide Web Consortium',
+      'World Web Writers Company'
+    ]
+  }
+];
+
+MultipleChoice(ActivityRunner(), questions);
