2312fb5c427583bd9c679aafddc95bd5d80e78a3
Thomas J Bradley <hey@thomasjbradley.ca>
2016-09-29 13:04:47 -0400

    Add the Specificity Spy activity

---
+++ specificity-spy/activity.js
@@ -0,0 +1,98 @@
+var questions = [
+  {
+    question: 'What colour will the text be?',
+    extras: 'question-1',
+    correct: 1,
+    choices: [
+      '`red`',
+      '`green`',
+    ]
+  },
+  {
+    question: 'What colour will the text be?',
+    extras: 'question-2',
+    correct: 1,
+    choices: [
+      '`orange`',
+      '`yellow`',
+    ]
+  },
+  {
+    question: 'What colour will the text be?',
+    extras: 'question-3',
+    correct: 1,
+    choices: [
+      '`pink`',
+      '`magenta`',
+    ]
+  },
+  {
+    question: 'What colour will the text be?',
+    extras: 'question-4',
+    correct: 1,
+    choices: [
+      '`green`',
+      '`limegreen`',
+    ]
+  },
+  {
+    question: 'What colour will the text be?',
+    extras: 'question-5',
+    correct: 1,
+    choices: [
+      '`lavender`',
+      '`indigo`',
+    ]
+  },
+  {
+    question: 'What colour will the text be?',
+    extras: 'question-6',
+    correct: 2,
+    choices: [
+      '`burlywood`',
+      '`goldenrod`',
+      '`chocolate`',
+    ]
+  },
+  {
+    question: 'What colour will the text be?',
+    extras: 'question-7',
+    correct: 1,
+    choices: [
+      '`gainsboro`',
+      '`grey`',
+      '`darkgrey`',
+    ]
+  },
+  {
+    question: 'What colour will the text be?',
+    extras: 'question-8',
+    correct: 2,
+    choices: [
+      '`darkgreen`',
+      '`darkolivegreen`',
+      '`forestgreen`',
+    ]
+  },
+  {
+    question: 'What colour will the text be?',
+    extras: 'question-9',
+    correct: 2,
+    choices: [
+      '`darkred`',
+      '`lightcoral`',
+      '`firebrick`',
+    ]
+  },
+  {
+    question: 'What colour will the text be?',
+    extras: 'question-10',
+    correct: 0,
+    choices: [
+      '`deepskyblue`',
+      '`lightskyblue`',
+    ]
+  }
+];
+
+MultipleChoice(ActivityRunner(), questions);
