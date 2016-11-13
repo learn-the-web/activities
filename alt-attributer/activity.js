50b9813e65f0cf27be4064e679c936b520687632
Thomas J Bradley <hey@thomasjbradley.ca>
2016-11-13 16:16:36 -0500

    Add the Alt Attributer activity

---
+++ alt-attributer/activity.js
@@ -0,0 +1,105 @@
+var questions = [
+  {
+    question: 'What should the `alt` attribute be?',
+    extras: 'question-1',
+    correct: 2,
+    choices: [
+      '`alt="logo"`',
+      '`alt="Logo for Ghosts’R’Us"`',
+      '`alt="Ghosts’R’Us"`',
+      '`alt=""`'
+    ]
+  },
+  {
+    question: 'What should the `alt` attribute be?',
+    extras: 'question-2',
+    correct: 0,
+    choices: [
+      '`alt="Babs hiding her eyes as a shadow of the bogey-owl follows her"`',
+      '`alt="Illustration"`',
+      '`alt=""`',
+    ]
+  },
+  {
+    question: 'What should the `alt` attribute be?',
+    extras: 'question-3',
+    correct: 2,
+    choices: [
+      '`alt="Learn the ways of dragon wrangling"`',
+      '`alt="A woodcut of a knight battling an evil dragon"`',
+      '`alt=""`',
+    ]
+  },
+  {
+    question: 'What should the `alt` attributes be?',
+    extras: 'question-4',
+    correct: 2,
+    choices: [
+      '`alt="Ghost"`',
+      '`alt="A ghost looking spookily onwards"`',
+      '`alt=""`',
+    ]
+  },
+  {
+    question: 'What should the `alt` attribute be?',
+    extras: 'question-5',
+    correct: 2,
+    choices: [
+      '`alt="Monsters of the air and deep leaving the port of Palos, they had been constantly on the watch"`',
+      '`alt="All the monsters"`',
+      '`alt=""`',
+    ]
+  },
+  {
+    question: 'What should the `alt` attribute be?',
+    extras: 'question-6',
+    correct: 0,
+    choices: [
+      '`alt="Monsters of the air and deep leaving the port of Palos, they had been constantly on the watch"`',
+      '`alt="All the monsters"`',
+      '`alt=""`',
+    ]
+  },
+  {
+    question: 'What should the `alt` attribute be?',
+    extras: 'question-7',
+    correct: 2,
+    choices: [
+      '`alt="Company logo"`',
+      '`alt="Company name in hairy monster text"`',
+      '`alt="Harry Monster’s Emporium for Hairy Monsters"`',
+    ]
+  },
+  {
+    question: 'What should the `alt` attribute be?',
+    extras: 'question-8',
+    correct: 0,
+    choices: [
+      '`alt="Monster attacks — 1775: 1.8M, 1775: 2.0M, 1777: 2.4M, 1778: 3.1M, 1779: 3.8M"`',
+      '`alt="A bar graph denoting the a steady increase in monster attacks since 1775"`',
+      '`alt=""`',
+    ]
+  },
+  {
+    question: 'What should the `alt` attribute be?',
+    extras: 'question-9',
+    correct: 1,
+    choices: [
+      '`alt="Monster attacks — 1775: 1.8M, 1775: 2.0M, 1777: 2.4M, 1778: 3.1M, 1779: 3.8M"`',
+      '`alt="A bar graph denoting the a steady increase in monster attacks since 1775"`',
+      '`alt=""`',
+    ]
+  },
+  {
+    question: 'What should the `alt` attribute be?',
+    extras: 'question-10',
+    correct: 2,
+    choices: [
+      '`alt="The Sea Fairies by Frank L. Baum"`',
+      '`alt=""`',
+      'No `alt` — background images are purely decoration',
+    ]
+  }
+];
+
+MultipleChoice(ActivityRunner(), questions);
