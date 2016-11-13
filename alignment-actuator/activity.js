01e23f47385b9bc52e66fdae5b9a6ce5ccf5ac21
Thomas J Bradley <hey@thomasjbradley.ca>
2016-11-13 12:44:42 -0500

    Add the Alignment Actuator activity

---
+++ alignment-actuator/activity.js
@@ -0,0 +1,17 @@
+var
+  questions = [],
+  incorrectSamples = document.querySelectorAll('[data-question]'),
+  i = 0, t = incorrectSamples.length
+;
+
+for (i; i<t; i++) {
+  var id = incorrectSamples[i].getAttribute('data-question');
+
+  questions.push({
+    question: 'Correct the indentation issues.',
+    incorrect: incorrectSamples[i].innerHTML,
+    correct: document.querySelector('[data-answer="' + id + '"]').innerHTML
+  });
+}
+
+CodeCompare(ActivityRunner(), questions);
