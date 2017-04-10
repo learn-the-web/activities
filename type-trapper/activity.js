97489e30659f4f51f4fa73c21ca8ee381c491cf5
Thomas J Bradley <hey@thomasjbradley.ca>
2017-04-10 14:49:49 -0400

    Add the Type Trapper quiz

---
+++ type-trapper/activity.js
@@ -0,0 +1,199 @@
+var questions = [
+  {
+    question: 'What data type is the `name` variable?',
+    extras: 'question-1',
+    correct: 0,
+    choices: [
+      'String',
+      'Number',
+      'Boolean',
+      'Array',
+      'Object',
+      'Function',
+    ]
+  },
+  {
+    question: 'What data type is the `age` variable?',
+    extras: 'question-2',
+    correct: 1,
+    choices: [
+      'String',
+      'Number',
+      'Boolean',
+      'Array',
+      'Object',
+      'Function',
+    ]
+  },
+  {
+    question: 'What data type is the `isPlantEater` variable?',
+    extras: 'question-3',
+    correct: 2,
+    choices: [
+      'String',
+      'Number',
+      'Boolean',
+      'Array',
+      'Object',
+      'Function',
+    ]
+  },
+  {
+    question: 'What data type is the `species` variable?',
+    extras: 'question-4',
+    correct: 3,
+    choices: [
+      'String',
+      'Number',
+      'Boolean',
+      'Array',
+      'Object',
+      'Function',
+    ]
+  },
+  {
+    question: 'What data type is the `details` variable?',
+    extras: 'question-5',
+    correct: 4,
+    choices: [
+      'String',
+      'Number',
+      'Boolean',
+      'Array',
+      'Object',
+      'Function',
+    ]
+  },
+  {
+    question: 'What data type is each item inside the array?',
+    extras: 'question-6',
+    correct: 0,
+    choices: [
+      'String',
+      'Number',
+      'Boolean',
+      'Array',
+      'Object',
+      'Function',
+    ]
+  },
+  {
+    question: 'What data type is the `height` property?',
+    extras: 'question-7',
+    correct: 1,
+    choices: [
+      'String',
+      'Number',
+      'Boolean',
+      'Array',
+      'Object',
+      'Function',
+    ]
+  },
+  {
+    question: 'What data type is the `swingTail` variable?',
+    extras: 'question-8',
+    correct: 5,
+    choices: [
+      'String',
+      'Number',
+      'Boolean',
+      'Array',
+      'Object',
+      'Function',
+    ]
+  },
+  {
+    question: 'What data type is the `console` variable?',
+    extras: 'question-9',
+    correct: 4,
+    choices: [
+      'String',
+      'Number',
+      'Boolean',
+      'Array',
+      'Object',
+      'Function',
+    ]
+  },
+  {
+    question: 'What data type is the `.log()` property?',
+    extras: 'question-10',
+    correct: 5,
+    choices: [
+      'String',
+      'Number',
+      'Boolean',
+      'Array',
+      'Object',
+      'Function',
+    ]
+  },
+  {
+    question: "What data type is the `'Smack!'` bit?",
+    extras: 'question-11',
+    correct: 0,
+    choices: [
+      'String',
+      'Number',
+      'Boolean',
+      'Array',
+      'Object',
+      'Function',
+    ]
+  },
+  {
+    question: 'What data type is the `elems` variable?',
+    extras: 'question-12',
+    correct: 3,
+    choices: [
+      'String',
+      'Number',
+      'Boolean',
+      'Array',
+      'Object',
+      'Function',
+    ]
+  },
+  {
+    question: 'What data type is stored inside the `elems[2]` item?',
+    extras: 'question-13',
+    correct: 4,
+    choices: [
+      'String',
+      'Number',
+      'Boolean',
+      'Array',
+      'Object',
+      'Function',
+    ]
+  },
+  {
+    question: 'What data type is the `.style` property?',
+    extras: 'question-14',
+    correct: 4,
+    choices: [
+      'String',
+      'Number',
+      'Boolean',
+      'Array',
+      'Object',
+      'Function',
+    ]
+  },
+  {
+    question: 'What data type is the `.backgroundColor` property?',
+    extras: 'question-15',
+    correct: 0,
+    choices: [
+      'String',
+      'Number',
+      'Boolean',
+      'Array',
+      'Object',
+      'Function',
+    ]
+  }
+];
+
+MultipleChoice(ActivityRunner(), questions);
