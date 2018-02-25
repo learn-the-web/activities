e8165b1977df3652a149d0059da062fe28e2911c
Thomas J Bradley <hey@thomasjbradley.ca>
2018-02-25 10:32:02 -0500

    Add the Variable Validator quiz

---
+++ variable-validator/activity.js
@@ -0,0 +1,106 @@
+var questions = [
+  {
+    question: 'What colour will the `<h1>` tag’s background be?',
+    extras: 'question-1',
+    correct: 0,
+    choices: [
+      '`red`',
+      '`black`',
+      '`transparent`',
+    ]
+  },
+  {
+    question: 'What colour will the `<h1>` tag’s background be?',
+    extras: 'question-2',
+    correct: 2,
+    choices: [
+      '`red`',
+      '`black`',
+      '`transparent`',
+    ]
+  },
+  {
+    question: 'What colour will the `<h1>` tag’s background be?',
+    extras: 'question-3',
+    correct: 2,
+    choices: [
+      '`yellow`',
+      '`black`',
+      '`transparent`',
+    ]
+  },
+  {
+    question: 'What colour will the `<h1>` tag’s background be?',
+    extras: 'question-4',
+    correct: 1,
+    choices: [
+      '`yellow`',
+      '`orange`',
+      '`transparent`',
+    ]
+  },
+  {
+    question: 'What colour will the `<h1>` tag’s background be?',
+    extras: 'question-5',
+    correct: 0,
+    choices: [
+      '`yellow`',
+      '`orange`',
+      '`transparent`',
+    ]
+  },
+  {
+    question: 'What colour will the `<h1>` tag’s background be?',
+    extras: 'question-6',
+    correct: 1,
+    choices: [
+      '`yellow`',
+      '`magenta`',
+      '`transparent`',
+    ]
+  },
+  {
+    question: 'What colour will the `<h1>` tag’s background be?',
+    extras: 'question-7',
+    correct: 1,
+    choices: [
+      '`yellow`',
+      '`magenta`',
+      '`transparent`',
+    ]
+  },
+  {
+    question: 'What colour will the `<h1>` tag’s background be?',
+    extras: 'question-8',
+    correct: 1,
+    choices: [
+      '`yellow`',
+      '`magenta`',
+      '`transparent`',
+    ]
+  },
+  {
+    question: 'What colour will the `<h1>` tag’s background be?',
+    extras: 'question-9',
+    correct: 2,
+    choices: [
+      '`yellow`',
+      '`orange`',
+      '`magenta`',
+      '`transparent`',
+    ]
+  },
+  {
+    question: 'What colour will the `<h1>` tag’s background be?',
+    extras: 'question-10',
+    correct: 2,
+    choices: [
+      '`yellow`',
+      '`magenta`',
+      '`grey`',
+      '`transparent`',
+    ]
+  },
+];
+
+MultipleChoice(ActivityRunner(), questions);
